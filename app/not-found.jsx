8679c951681485278f8711124c483e0fd3da6bb7
yopaaa <pusi.ng027@gmail.com>
2026-05-23 16:02:17 +0700

    update css

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,34 +1,43 @@
-"use client"
+"use client";
 
-import styles from '@/styles/error.module.css';
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-export default function Custom404() {
-   const router = useRouter();
+import Header from "@/components/Layout/Header";
+import Footer from "@/components/Layout/Footer";
+import styles from "./page.module.css"; // Memakai layout.module.css
+
+export default function RegisterLayout({ children }) {
+  const router = useRouter();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.center}>
-        <h1 className={styles.title}>
-          404
-        </h1>
-        <p className={styles.heading}>
-          Halaman tidak ditemukan.
-        </p>
-        <p className={styles.description}>
-          Sepertinya halaman yang Anda cari tidak ada atau telah dihapus.
-        </p>
+    <div className={styles.pageWrapper}>
+      {/* Ornamen Latar Belakang Tetap di Sini */}
 
-         <button className={styles.button} onClick={() => router.back()}>
-          Kembali ke Sebelumnya
-        </button>
+      {/* Header Global */}
+      <div className={styles.contentWrapper}>
+        <Header />
+
+        <div className={styles.container}>
+          <div className={styles.center}>
+            <h1 className={styles.title}>404</h1>
+            <p className={styles.heading}>Halaman tidak ditemukan.</p>
+            <p className={styles.description}>
+              Sepertinya halaman yang Anda cari tidak ada atau telah dihapus.
+            </p>
+
+            <button className={styles.button} onClick={() => router.back()}>
+              Kembali ke Sebelumnya
+            </button>
+          </div>
+
+          <div className={styles.footer}>
+            <p>.</p>
+          </div>
+        </div>
       </div>
 
-      <div className={styles.footer}>
-        <p>
-          .
-        </p>
-      </div>
+      {/* Footer Global */}
+      <Footer />
     </div>
   );
 }
