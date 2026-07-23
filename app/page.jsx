c92413cdc54eeaead53d2c1e62ebacc31b082f5a
yopaaa <pusi.ng027@gmail.com>
2026-07-23 23:24:09 +0700

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import LecturerShowcase from "../components/Layout/LecturerShowcase";
 import MaterialList from "@/components/UI/MaterialList";
 import HeroSection from "@/components/UI/HeroSection";
 
-export const revalidate = 60 * 60; // Revalidate setiap 1 jam
+export const revalidate = 3600;
 
 export default function RegisterLayout({ children }) {
   return (
